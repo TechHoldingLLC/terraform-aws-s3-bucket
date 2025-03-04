4b349691f6526a5fabb51f5450fbd59591f9df15
Kandarp Dave <kandarp.dave@techholding.co>
2025-03-04 16:46:19 +0530

    Fix: json concatination errors

--- bucket-policy.tf
+++ bucket-policy.tf
@@ -1,14 +1,17 @@
 ## s3 cloudfront policy for origin access control
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  count = (var.origin_access_control || var.block_http_request || var.bucket_policy) != null ? 1 : 0
+  count = (var.origin_access_control || var.block_http_request || var.bucket_policy != null) ? 1 : 0
 
   bucket = aws_s3_bucket.s3.id
 
-  policy = jsonencode(compact([
-    var.origin_access_control ? data.aws_iam_policy_document.s3_cloudfront_oac[0].json : "",
-    var.block_http_request ? data.aws_iam_policy_document.block_http_request[0].json : "",
-    var.bucket_policy != null ? var.bucket_policy : ""
-  ]))
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = concat(
+      var.origin_access_control ? jsondecode(data.aws_iam_policy_document.s3_cloudfront_oac[0].json).Statement : [],
+      var.block_http_request ? jsondecode(data.aws_iam_policy_document.block_http_request[0].json).Statement : [],
+      var.bucket_policy != null ? jsondecode(var.bucket_policy).Statement : []
+    )
+  })
 }
 
 ## s3 cloudfront policy document for origin access control
