6ccc75d0ffd594e4d37b316e27ba5ec0aa337e0d
Kandarp Dave <137057311+kandarpdave3@users.noreply.github.com>
2025-03-04 17:19:53 +0530

    Fix: terraform plan error for bucket policy due to count (#9)
    * Fix: terraform plan error for bucket policy due to count
    
    * Fix: json concatination errors

--- bucket-policy.tf
+++ bucket-policy.tf
@@ -1,18 +1,17 @@
 ## s3 cloudfront policy for origin access control
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  count  = var.origin_access_control || var.block_http_request || var.bucket_policy != null ? 1 : 0
+  count = (var.origin_access_control || var.block_http_request || var.bucket_policy != null) ? 1 : 0
+
   bucket = aws_s3_bucket.s3.id
-  policy = data.aws_iam_policy_document.combined[0].json
-}
 
-data "aws_iam_policy_document" "combined" {
-  count = var.origin_access_control || var.block_http_request || var.bucket_policy != null ? 1 : 0
-
-  source_policy_documents = compact([
-    var.origin_access_control ? data.aws_iam_policy_document.s3_cloudfront_oac[0].json : "",
-    var.block_http_request ? data.aws_iam_policy_document.block_http_request[0].json : "",
-    var.bucket_policy != null ? var.bucket_policy : ""
-  ])
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = concat(
+      var.origin_access_control ? jsondecode(data.aws_iam_policy_document.s3_cloudfront_oac[0].json).Statement : [],
+      var.block_http_request ? jsondecode(data.aws_iam_policy_document.block_http_request[0].json).Statement : [],
+      var.bucket_policy != null ? jsondecode(var.bucket_policy).Statement : []
+    )
+  })
 }
 
 ## s3 cloudfront policy document for origin access control
